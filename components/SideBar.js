87927de14d26f7d7c94e3f830a3594366dde98fb
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-05-09 02:14:21 +0300

    Adding styled components

---
+++ components/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div className=''>
+        SideBar
+    </div>
+  );
+}
+
+export default SideBar;
+
+const container = styled.div`
+
+`;
---
+++ pages/chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function chat() {
+  return (
+    <div className=''>
+        <h2>This is a chat page </h2>
+    </div>
+  );
+}
+
+export default chat;
