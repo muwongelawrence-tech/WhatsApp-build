9d7c8df2c7ecd42e2bf0395eef8291ee28c07230
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-05-18 21:10:22 +0300

    Adding firebase

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
 import firebase from firebase;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnNkZMtBMdlqfDMIV4YXmkP0nOPL6VMBc",
+    authDomain: "whats-app-2-46e4b.firebaseapp.com",
+    projectId: "whats-app-2-46e4b",
+    storageBucket: "whats-app-2-46e4b.appspot.com",
+    messagingSenderId: "772359068770",
+    appId: "1:772359068770:web:2e4550130882332672453b"
+};
+
+  const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig)
+            : firebase.app();
