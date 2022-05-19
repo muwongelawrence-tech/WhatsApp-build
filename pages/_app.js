eb261fbbf9cb420037d24041000c37e31f295ba4
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-05-19 13:21:08 +0300

    Adding useEffect for loading

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,29 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth , db } from "../firebase";
 import Login from './Login';
 import Loading from './Loading';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
 
   const [user , loading ] = useAuthState(auth);
+
+  useEffect(() => {
+       
+    if(user){
+      db.collection('users').doc(user.uid).set({
+         email:user.email,
+         lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+         photoURL: user.photoURL
+        },
+
+        { merge: true }
+        
+      );
+    }
+
+  }, [user]);
 
   if(loading) return <Loading/>
 
