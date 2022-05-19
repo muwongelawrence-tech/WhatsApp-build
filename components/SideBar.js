2e47b826e28d2acbae4f2dcfa053eaebb9133271
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-05-19 13:53:33 +0300

    checking for existing chats

--- components/SideBar.js
+++ components/SideBar.js
@@ -5,9 +5,16 @@ import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
 import * as EmailValidator from "email-validator";
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 function SideBar() {
+
+  const [user] = useAuthState(auth);
+
+  const userChatRef = db.collection('chats').where('users', 'array-contains', user.email);
+  const [ chatsSnapshot] = useCollection(userChatRef);
 
   const createChat = () => {
 
@@ -17,13 +24,22 @@ function SideBar() {
 
     if(!input) return null;
 
-    if(EmailValidator.validate(input)){
+    if(EmailValidator.validate(input) && input !== user.email ){
 
       // adding the chats into the DB or "chats " collection
+      db.collection("chats").add({
+             users: [ user.email , input],
+      })
       
 
     }
 
+  }
+
+  const chatAlreadyExists = (recipientEmail) => {
+     chatsSnapshot?.docs.find(
+       (chat) => chat.data().users.find((user) => user === recipientEmail)?.length > 0
+     );
   }
 
   return (
