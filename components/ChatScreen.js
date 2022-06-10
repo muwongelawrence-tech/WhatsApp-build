458df4ea3b3b6b2515aac753ff8c08ab25ef2197
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-06-11 02:58:03 +0300

    building the chart screen

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,10 +1,38 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import styled from "styled-components";
+import { auth } from '../firebase';
 
 function ChatScreen({ chat, messages}) {
+
+  const [ user ] = useAuthState(auth); 
+  const router = useRouter();
+
   return (
     <Container>
-        <div> This is a ChatScreen</div>
+        <Header>
+             <Avatar />
+
+             <HeaderInformation>
+                 <h3> rec Email</h3>
+                 <p>Last seen ...</p>
+             </HeaderInformation>
+
+             <HeaderIcons>
+
+             <IconButton>
+                   <AttachFileIcon/>
+               </IconButton>
+
+               <IconButton>
+                   <MoreVertIcon/>
+               </IconButton>
+
+             </HeaderIcons>
+
+        </Header>
     </Container>
   );
 }
@@ -12,3 +40,15 @@ function ChatScreen({ chat, messages}) {
 export default ChatScreen;
 
 const Container = styled.div``;
+
+const Header = styled.div`
+
+`;
+
+const HeaderInformation = styled.div`
+
+`;
+
+const HeaderIcons = styled.div`
+
+`;
